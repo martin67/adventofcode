d52077f67bb017bd40f0c6f225b6d4736f14e084
Martin Hagelin <martin@hagelin.nu>
2019-04-10 16:50:43 +0200

    Fixed second problem of day 1

--- src/main/java/Day1ChronalCalibration.java
+++ src/main/java/Day1ChronalCalibration.java
@@ -1,9 +1,32 @@
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day1ChronalCalibration {
     int computeFrequency(String input) {
         return Arrays.stream(input.trim().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .sum();
+    }
+
+    int computeDoubleFrequency(String input) {
+        Set<Integer> previousFrequencies = new HashSet<>();
+
+        List<Integer> changes = Arrays.stream(input.trim().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int sum = 0;
+        previousFrequencies.add(0);
+
+        while(true) {
+            for (int i : changes) {
+
+                sum += i;
+                if (previousFrequencies.contains(sum)) {
+                    return sum;
+                } else {
+                    previousFrequencies.add(sum);
+                }
+            }
+        }
     }
 }
