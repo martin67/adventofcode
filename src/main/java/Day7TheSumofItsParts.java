419d17b2faf84a760dff4e7c59a076d8ec4be9a4
martin67 <martin@hagelin.nu>
2019-04-15 10:48:45 +0200

    Day 7 part 1

---
+++ src/main/java/Day7TheSumofItsParts.java
@@ -0,0 +1,76 @@
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class Day7TheSumofItsParts {
+
+    @Data
+    @RequiredArgsConstructor
+    class Step {
+        final String name;
+        List<Step> beforeSteps = new ArrayList<>();
+        boolean available = true;
+    }
+
+    List<Step> stepList = new ArrayList<>();
+
+
+    void readData(String input) {
+        // Split string into a list
+        List<String> inputStrings = Arrays.stream(input.trim().split("\\n")).collect(Collectors.toList());
+        for (String string : inputStrings) {
+            String before = StringUtils.substringBetween(string, "Step ", " must");
+            String after = StringUtils.substringBetween(string, "step ", " can");
+
+            // See if the before step exist in the list
+            Step beforeStep = stepList.stream().filter(s -> s.getName().equals(before)).findAny().orElse(null);
+            if (beforeStep == null) {
+                beforeStep = new Step(before);
+                stepList.add(beforeStep);
+            }
+            Step afterStep = stepList.stream().filter(s -> s.getName().equals(after)).findAny().orElse(null);
+            if (afterStep == null) {
+                afterStep = new Step(after);
+                stepList.add(afterStep);
+            }
+            afterStep.getBeforeSteps().add(beforeStep);
+        }
+        log.info("Read " + inputStrings.size() + " lines, creating " + stepList.size() + " steps");
+    }
+
+
+    String puzzleOrder(String input) {
+        readData(input);
+
+        StringBuilder result = new StringBuilder();
+
+        Step aStep;
+        do {
+            aStep = stepList.stream().
+                    filter(s -> s.isAvailable()).
+                    filter(s -> s.getBeforeSteps().isEmpty()).
+                    sorted(Comparator.comparing(Step::getName)).findAny().orElse(null);
+            // Process aStep
+            if (aStep != null) {
+                // add to result
+                result.append(aStep.getName());
+                // make it not available (as it has been done)
+                aStep.setAvailable(false);
+                // Remove it from all before steps (as it is no longer a preq)
+                Step finalAStep = aStep;
+                stepList.stream().filter(s -> s.getBeforeSteps().contains(finalAStep)).forEach(s -> s.getBeforeSteps().remove(finalAStep));
+            }
+        }
+        while (aStep != null);
+
+        return result.toString();
+    }
+}
---
+++ src/test/java/Day7TheSumofItsPartsTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day7TheSumofItsPartsTest {
+
+    @Test
+    void puzzleOrder() {
+            String input = "Step C must be finished before step A can begin.\n" +
+                "Step C must be finished before step F can begin.\n" +
+                "Step A must be finished before step B can begin.\n" +
+                "Step A must be finished before step D can begin.\n" +
+                "Step B must be finished before step E can begin.\n" +
+                "Step D must be finished before step E can begin.\n" +
+                "Step F must be finished before step E can begin.\n";
+            Day7TheSumofItsParts day7TheSumofItsParts = new Day7TheSumofItsParts();
+            assertEquals("CABDFE", day7TheSumofItsParts.puzzleOrder(input));
+    }
+
+    @Test
+    void puzzleOrderFromFile() throws IOException {
+        String input = new String((Files.readAllBytes(Paths.get("build/resources/test/day7.txt"))));
+        Day7TheSumofItsParts day7TheSumofItsParts = new Day7TheSumofItsParts();
+        assertEquals("ABGKCMVWYDEHFOPQUILSTNZRJX", day7TheSumofItsParts.puzzleOrder(input));
+    }
+
+}
