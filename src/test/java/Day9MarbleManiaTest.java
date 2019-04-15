62a54c9e64296ced05f0b9132f42510f3aedcdfe
martin67 <martin@hagelin.nu>
2019-04-16 00:48:26 +0200

    Day 9 part 1 solved

---
+++ src/test/java/Day9MarbleManiaTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day9MarbleManiaTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "9, 25, 32",
+            "10, 1618, 8317",
+            "13, 7999, 146373",
+            "17, 1104, 2764",
+            "21, 6111, 54718",
+            "30, 5807, 37305",
+            "424, 71144, 405143",
+            "424, 7114400, 0"
+    })
+    void computeScore(int players, int points, int expected) {
+        Day9MarbleMania day9MarbleMania = new Day9MarbleMania();
+        assertEquals(expected, day9MarbleMania.computeScore(players, points));
+    }
+
+}
