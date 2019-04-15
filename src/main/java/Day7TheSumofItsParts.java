91147e76e19b1a7280648696c408d84d0fc14894
martin67 <martin@hagelin.nu>
2019-04-15 12:18:34 +0200

    Day 7 part 2 solved

--- src/main/java/Day7TheSumofItsParts.java
+++ src/main/java/Day7TheSumofItsParts.java
@@ -19,6 +19,7 @@ public class Day7TheSumofItsParts {
         final String name;
         List<Step> beforeSteps = new ArrayList<>();
         boolean available = true;
+        boolean busy = false;
         @NonNull
         int timeLeft;
     }
@@ -26,7 +27,7 @@ public class Day7TheSumofItsParts {
     List<Step> stepList = new ArrayList<>();
 
 
-    void readData(String input) {
+    void readData(String input, int duration) {
         // Split string into a list
         List<String> inputStrings = Arrays.stream(input.trim().split("\\n")).collect(Collectors.toList());
         for (String string : inputStrings) {
@@ -36,12 +37,12 @@ public class Day7TheSumofItsParts {
             // See if the before step exist in the list
             Step beforeStep = stepList.stream().filter(s -> s.getName().equals(before)).findAny().orElse(null);
             if (beforeStep == null) {
-                beforeStep = new Step(before, before.charAt(0) - 4);
+                beforeStep = new Step(before, before.charAt(0) - 64 + duration);
                 stepList.add(beforeStep);
             }
             Step afterStep = stepList.stream().filter(s -> s.getName().equals(after)).findAny().orElse(null);
             if (afterStep == null) {
-                afterStep = new Step(after, after.charAt(0) - 4);
+                afterStep = new Step(after, after.charAt(0) - 64 + duration);
                 stepList.add(afterStep);
             }
             afterStep.getBeforeSteps().add(beforeStep);
@@ -51,7 +52,7 @@ public class Day7TheSumofItsParts {
 
 
     String puzzleOrder(String input) {
-        readData(input);
+        readData(input, 0);
 
         StringBuilder result = new StringBuilder();
 
@@ -78,9 +79,50 @@ public class Day7TheSumofItsParts {
     }
 
 
-    int computeTime(String input) {
-        readData(input);
+    int computeTime(String input, int numberOfWorkers, int duration) {
+        readData(input, duration);
+        StringBuilder result = new StringBuilder();
+        int totalTime = 0;
 
-        return 0;
+        Step aStep = null;
+        do {
+            // Find out how many free workers available
+            int freeWorkers = numberOfWorkers - (int) stepList.stream().filter(s -> s.busy).count();
+            log.info("Second " + totalTime + ", " + freeWorkers + " free workers");
+
+            // assign work for each free worker
+            for (int i = 0; i < freeWorkers; i++) {
+                aStep = stepList.stream().
+                        filter(s -> s.isAvailable()).
+                        filter(s -> !s.isBusy()).
+                        filter(s -> s.getBeforeSteps().isEmpty()).
+                        sorted(Comparator.comparing(Step::getName)).findAny().orElse(null);
+                // Process aStep
+                if (aStep != null) {
+                    aStep.setBusy(true);
+                    log.info("Assigning one worker to step " + aStep.getName());
+                }
+            }
+
+            // Decrease time left for all busy steps
+            stepList.stream().filter(s -> s.isBusy()).forEach(s -> s.timeLeft--);
+
+            // Check of any busy step is finished
+            // in that case, remove it
+            stepList.stream().filter(s -> s.isBusy()).
+                    filter(s -> s.getTimeLeft() == 0).
+                    forEach(s -> {
+                        result.append(s.getName());
+                        log.info("Step " +  s.getName() + " finished");
+                        s.setBusy(false);
+                        s.setAvailable(false);
+                        Step finalAStep = s;
+                        stepList.stream().filter(s2 -> s2.getBeforeSteps().contains(finalAStep)).forEach(s2 -> s2.getBeforeSteps().remove(finalAStep));
+                    });
+
+            totalTime++;
+        } while (stepList.stream().filter(s -> s.isAvailable()).count() > 0);
+
+        return totalTime;
     }
 }
--- src/test/java/Day7TheSumofItsPartsTest.java
+++ src/test/java/Day7TheSumofItsPartsTest.java
@@ -38,6 +38,13 @@ class Day7TheSumofItsPartsTest {
                 "Step D must be finished before step E can begin.\n" +
                 "Step F must be finished before step E can begin.\n";
         Day7TheSumofItsParts day7TheSumofItsParts = new Day7TheSumofItsParts();
-        assertEquals(15, day7TheSumofItsParts.computeTime(input));
+        assertEquals(15, day7TheSumofItsParts.computeTime(input, 2, 0));
+    }
+
+    @Test
+    void computeTimeFromFile() throws IOException {
+        String input = new String((Files.readAllBytes(Paths.get("build/resources/test/day7.txt"))));
+        Day7TheSumofItsParts day7TheSumofItsParts = new Day7TheSumofItsParts();
+        assertEquals(898, day7TheSumofItsParts.computeTime(input, 5, 60));
     }
 }
