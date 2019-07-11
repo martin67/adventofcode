6b9a0862f4533c8dedc3b5add257c40e22f73291
Martin Hagelin <martin@hagelin.nu>
2019-07-11 14:13:33 +0200

    Day 12 first part

--- src/main/java/Day12SubterraneanSustainability.java
+++ src/main/java/Day12SubterraneanSustainability.java
@@ -2,10 +2,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -25,12 +22,14 @@ public class Day12SubterraneanSustainability {
     @AllArgsConstructor
     class Note {
         String pattern;
-        char state;
+        char nextState;
     }
 
+    @Data
     class Tunnel {
-        List<Pot> Pots = new LinkedList<>();
-        List<Note> Notes = new ArrayList<>();
+        LinkedList<Pot> pots = new LinkedList<>();
+        List<Note> notes = new ArrayList<>();
+        int potOffset;
 
         void init(String input) {
             List<String> inputStrings = Arrays.stream(input.trim().split("\\n+"))
@@ -40,32 +39,91 @@ public class Day12SubterraneanSustainability {
             // initial state: #..#.#..##......###...###
             for (char c : inputStrings.get(0).toCharArray()) {
               if (c == '.' || c == '#') {
-                  Pots.add(new Pot(c));
+                  pots.add(new Pot(c));
               }
             }
-            log.info("Reading " + Pots.size() + " states.");
+            log.info("Reading " + pots.size() + " states.");
 
             // Then it's the growth patterns
             // ...## => #
             for (String line : inputStrings.subList(1, inputStrings.size())) {
-                Notes.add(new Note( line.substring(0,5), line.charAt(9)));
+                notes.add(new Note( line.substring(0,5), line.charAt(9)));
             }
-            log.info("Reading " + Notes.size() + " notes.");
+            log.info("Reading " + notes.size() + " notes.");
         }
 
         void grow() {
-            for (Pot pot : Pots) {
+            // add four empty pots to the left of the list and four to the right
+            for (int i = 0; i < 4; i++) {
+                pots.addFirst(new Pot('.'));
+            }
+            for (int i = 0; i < 4; i++) {
+                pots.addLast(new Pot('.'));
+            }
 
+            // Create a new temporary potlist to store results
+            LinkedList<Pot> newPots = new LinkedList<>();
+
+            // Start comparing patterns from pos 2 till end-2
+            for (int i = 2; i < pots.size() - 2; i++) {
+                //log.info("Checking pot " + (i-4) + " for pattern match");
+                Pot pot = pots.get(i);
+
+                Pot newPot = new Pot('.');
+                // Go through all patterns and check if there's a hit
+                for (Note note : notes) {
+                    if (note.getPattern().charAt(0) == pots.get(i - 2).getState() &&
+                            note.getPattern().charAt(1) == pots.get(i - 1).getState() &&
+                            note.getPattern().charAt(2) == pots.get(i).getState() &&
+                            note.getPattern().charAt(3) == pots.get(i + 1).getState() &&
+                            note.getPattern().charAt(4) == pots.get(i + 2).getState()) {
+                        //log.info("Found match on pot " + (i-4) + " for note: " + note);
+                        newPot.setState(note.getNextState());
+                    }
+                }
+
+                newPots.add(newPot);
+            }
+            pots = newPots;
+            trim();
+        }
+
+        void trim() {
+            // remove empty pots on each end
+            ListIterator<Pot> pi = pots.listIterator();
+            int potsAddedToStart = -2;
+            while (pi.hasNext() && pi.next().getState() == '.') {
+                pi.remove();
+                potsAddedToStart++;
+            }
+            //log.info("Pots added to start: " + potsAddedToStart);
+            potOffset += potsAddedToStart;
+
+            pi = pots.listIterator(pots.size());
+            while (pi.hasPrevious() && pi.previous().getState() == '.') {
+                pi.remove();
             }
         }
 
         void print() {
 
             System.out.printf("%2d: ", generation);
-            for(Pot pot: Pots) {
+            for(Pot pot: pots) {
                 System.out.print(pot.getState());
             }
+            System.out.printf("  Offset: %d", potOffset);
             System.out.println();
+        }
+
+        int sumOfPlants() {
+            int checksum = 0;
+
+            for (int i = 0; i < pots.size(); i++)
+                if (pots.get(i).getState() == '#') {
+                    checksum += i + potOffset;
+                    //log.info("Checksum: " + checksum + ", i: " + i);
+                }
+            return checksum;
         }
     }
 
@@ -80,7 +138,19 @@ public class Day12SubterraneanSustainability {
         for (int i = 0; i < 20; i++) {
             tunnel.grow();
             generation++;
+            log.info("potOffset: " + tunnel.getPotOffset());
             tunnel.print();
         }
+    }
+
+    public int ComputePlantSum() {
+        tunnel.print();
+        for (int i = 0; i < 20; i++) {
+            tunnel.grow();
+            generation++;
+            tunnel.print();
+        }
+
+        return tunnel.sumOfPlants();
     }
 }
--- src/test/java/Day12SubterraneanSustainabilityTest.java
+++ src/test/java/Day12SubterraneanSustainabilityTest.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Day12SubterraneanSustainabilityTest {
 
     @Test
-    void ReadData() throws IOException {
+    void ReadDemoData() throws IOException {
         String input = new String((Files.readAllBytes(Paths.get("build/resources/test/day12-1.txt"))));
         Day12SubterraneanSustainability day12SubterraneanSustainability = new Day12SubterraneanSustainability(input);
-        day12SubterraneanSustainability.CheckInitialState();
+        assertEquals(325, day12SubterraneanSustainability.ComputePlantSum());
+    }
+
+    @Test
+    void ReadData() throws IOException {
+        String input = new String((Files.readAllBytes(Paths.get("build/resources/test/day12-2.txt"))));
+        Day12SubterraneanSustainability day12SubterraneanSustainability = new Day12SubterraneanSustainability(input);
+        assertEquals(3061, day12SubterraneanSustainability.ComputePlantSum());
     }
 
 }
