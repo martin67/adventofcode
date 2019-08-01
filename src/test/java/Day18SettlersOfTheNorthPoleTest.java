c94062bddc7ded313091a1dcb4ad25261d8bdbc0
Martin Hagelin <martin@hagelin.nu>
2019-08-02 01:46:12 +0200

    Day 18, part 2 complete

--- src/test/java/Day18SettlersOfTheNorthPoleTest.java
+++ src/test/java/Day18SettlersOfTheNorthPoleTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,12 +12,18 @@ class Day18SettlersOfTheNorthPoleTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1147, day18-demo.txt",
-            "588436, day18.txt"
+            "1147, out/test/resources/day18-demo.txt",
+            "588436, out/test/resources/day18.txt"
     })
     void computeResourceValue(int outcome, String fileName) throws IOException {
-        String input = new String((Files.readAllBytes(Paths.get("out/test/resources/" + fileName))));
-        Day18SettlersOfTheNorthPole day18SettlersOfTheNorthPole = new Day18SettlersOfTheNorthPole(input);
+        Day18SettlersOfTheNorthPole day18SettlersOfTheNorthPole = new Day18SettlersOfTheNorthPole(fileName);
         assertEquals(outcome, day18SettlersOfTheNorthPole.computeResourceValue());
+    }
+
+    @Test
+    void totalResourceValue() throws IOException {
+        String fileName = "out/test/resources/day18.txt";
+        Day18SettlersOfTheNorthPole day18SettlersOfTheNorthPole = new Day18SettlersOfTheNorthPole(fileName);
+        assertEquals(195290, day18SettlersOfTheNorthPole.totalResourceValue());
     }
 }
