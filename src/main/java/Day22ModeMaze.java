c15b8d6864d48096ab4f562d6e2e616e5e501d82
martin67 <martin@hagelin.nu>
2019-09-02 23:22:07 +0200

    Day 22 finished part 1

---
+++ src/main/java/Day22ModeMaze.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+
+public class Day22ModeMaze {
+
+    static class Region {
+        Position position;
+        int geologicIndex;
+        int erosionLevel;
+        int type;
+
+        Region(Position position) {
+            this.position = position;
+        }
+    }
+
+    private HashMap<Position, Region> cave = new HashMap<>();
+    private Position target;
+
+    public Day22ModeMaze(int depth, Position target) {
+        this.target = new Position(target.x + 1, target.y + 1);
+        initCave(depth, this.target);
+    }
+
+    private void initCave(int depth, Position target) {
+
+        for (int x = 0; x < target.x; x++) {
+            for (int y = 0; y < target.y; y++) {
+                Region r = new Region(new Position(x, y));
+
+                if ((x == 0 && y == 0) || ((x == target.x - 1 && y == target.y - 1))) {
+                    r.geologicIndex = 0;
+                } else if (x == 0) {
+                    r.geologicIndex = y * 48271;
+                } else if (y == 0) {
+                    r.geologicIndex = x * 16807;
+                } else {
+                    r.geologicIndex = cave.get(new Position(x - 1, y)).erosionLevel *
+                            cave.get(new Position(x, y - 1)).erosionLevel;
+                }
+                r.erosionLevel = (r.geologicIndex + depth) % 20183;
+                r.type = r.erosionLevel % 3;
+                cave.put(r.position, r);
+            }
+        }
+    }
+
+    private void printCave() {
+        for (int y = 0; y < target.y; y++) {
+            for (int x = 0; x < target.x; x++) {
+                Region r = cave.get(new Position(x, y));
+                if (r.type == 0) {
+                    System.out.print('.');
+                } else if (r.type == 1) {
+                    System.out.print('=');
+                } else {
+                    System.out.print('|');
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public int computeRiskLevel() {
+        //printCave();
+
+        int riskLevel = 0;
+        for (Region r : cave.values()) {
+            riskLevel += r.type;
+        }
+        return riskLevel;
+    }
+
+}
---
+++ src/test/java/Day22ModeMazeTest.java
@@ -0,0 +1,21 @@
+import javafx.geometry.Pos;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day22ModeMazeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "114, 510, 10, 10",
+            "8681, 5616, 10, 785"
+    })
+    void riskLevel(int riskLevel, int depth, int x, int y) throws IOException {
+        Day22ModeMaze day22ModeMaze = new Day22ModeMaze(depth, new Position(x, y));
+        assertEquals(riskLevel, day22ModeMaze.computeRiskLevel());
+    }
+}
