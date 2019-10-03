3ee537349150f5af8b111ce409668b7e8dacfd5f
martin67 <martin@hagelin.nu>
2019-10-03 22:09:16 +0200

    Day 1 complete

---
+++ src/test/java/adventofcode2016/Day2BathroomSecurityTest.java
@@ -0,0 +1,25 @@
+package adventofcode2016;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day2BathroomSecurityTest {
+
+    @ParameterizedTest
+    @CsvSource({"1985, out/test/resources/2016/day2-demo1.txt",
+            "61529, out/test/resources/2016/day2.txt"})
+    void bathroomCode(String expected, String fileName) throws IOException {
+        assertEquals(expected, new Day2BathroomSecurity(fileName).bathroomCode());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5DB3, out/test/resources/2016/day2-demo1.txt",
+            "C2C28, out/test/resources/2016/day2.txt"})
+    void secondBathroomCode(String expected, String fileName) throws IOException {
+        assertEquals(expected, new Day2BathroomSecurity(fileName).secondBathroomCode());
+    }
+}
