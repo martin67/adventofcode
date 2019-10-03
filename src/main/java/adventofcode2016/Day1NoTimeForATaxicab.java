b6df9cfa709a4905ca75a79a331036582911b5a2
Martin Hagelin <martin@hagelin.nu>
2019-10-03 16:41:15 +0200

    Day 1

--- src/main/java/adventofcode2016/Day1NoTimeForATaxicab.java
+++ src/main/java/adventofcode2016/Day1NoTimeForATaxicab.java
@@ -1,11 +1,20 @@
 package adventofcode2016;
 
+import javafx.geometry.Pos;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day1NoTimeForATaxicab {
+
+    enum Direction {N, S, E, W}
+
+    private List<String> instructions = new ArrayList<>();
+    private List<Position> positionsVisited = new ArrayList<>();
 
     public Day1NoTimeForATaxicab(String fileName) throws IOException {
         readData(fileName);
@@ -15,11 +24,50 @@ public class Day1NoTimeForATaxicab {
         List<String> inputStrings = Files.readAllLines(Paths.get(fileName));
 
         for (String row : inputStrings) {
-
+            instructions.addAll(Arrays.asList(row.split("\\s*,\\s*")));
         }
     }
 
     int shortestPath() {
+        Position pos = new Position(0, 0);
+        Direction currentDirection = Direction.N;
+
+        for (String instruction : instructions) {
+            char turn = instruction.charAt(0);
+            int distance = Integer.parseInt(instruction.substring(1));
+            switch (currentDirection) {
+                case N:
+                    currentDirection = (turn == 'R') ? Direction.E : Direction.W;
+                    break;
+                case E:
+                    currentDirection = (turn == 'R') ? Direction.S : Direction.N;
+                    break;
+                case S:
+                    currentDirection = (turn == 'R') ? Direction.W : Direction.E;
+                    break;
+                case W:
+                    currentDirection = (turn == 'R') ? Direction.N : Direction.S;
+                    break;
+            }
+            switch (currentDirection) {
+                case N:
+                    pos.y -= distance;
+                    break;
+                case E:
+                    pos.x += distance;
+                    break;
+                case S:
+                    pos.y += distance;
+                    break;
+                case W:
+                    pos.x -= distance;
+                    break;
+            }
+        }
+        return pos.distance(new Position(0, 0));
+    }
+
+    int visitedTwice() {
         return 0;
     }
 }
--- src/test/java/adventofcode2016/Day1NoTimeForATaxicabTest.java
+++ src/test/java/adventofcode2016/Day1NoTimeForATaxicabTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day1NoTimeForATaxicabTest {
 
@@ -13,8 +13,15 @@ class Day1NoTimeForATaxicabTest {
     @CsvSource({"5, out/test/resources/2016/day1-demo1.txt",
             "2, out/test/resources/2016/day1-demo2.txt",
             "12, out/test/resources/2016/day1-demo3.txt",
-            "0, out/test/resources/2016/day1.txt"})
+            "250, out/test/resources/2016/day1.txt"})
     void shortestPath(int expected, String fileName) throws IOException {
         assertEquals(expected, new Day1NoTimeForATaxicab(fileName).shortestPath());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"4, out/test/resources/2016/day1-demo4.txt",
+            "0, out/test/resources/2016/day1.txt"})
+    void visitedTwice(int expected, String fileName) throws IOException {
+        assertEquals(expected, new Day1NoTimeForATaxicab(fileName).visitedTwice());
     }
 }
