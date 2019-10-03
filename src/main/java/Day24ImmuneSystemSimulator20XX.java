486f82ac876003aa40ae68e16c23591d731f916c
Martin Hagelin <martin@hagelin.nu>
2019-10-03 12:20:45 +0200

    Day 24, part 1 complete

--- src/main/java/Day24ImmuneSystemSimulator20XX.java
+++ src/main/java/Day24ImmuneSystemSimulator20XX.java
@@ -7,10 +7,23 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Day24ImmuneSystemSimulator20XX {
 
-    enum Side {immuneSystem, infection}
+    enum Side {
+        IMMUNE_SYSTEM {
+            @Override
+            public String toString() {
+                return "Immune System";
+            }
+        }, INFECTION {
+            @Override
+            public String toString() {
+                return "Infection";
+            }
+        }
+    }
 
     @Data
     @AllArgsConstructor
@@ -52,7 +65,7 @@ public class Day24ImmuneSystemSimulator20XX {
 
         @Override
         public String toString() {
-            return side + "/u=" + units + "/h=" + hitPoints + "/i=" + initiative + "/d=" + damage + "/" + attackType + "/" + weaknesses + "/" + immunities;
+            return side + "/id=" + id + "/u=" + units + "/h=" + hitPoints + "/i=" + initiative + "/d=" + damage + "/" + attackType + "/" + weaknesses + "/" + immunities;
         }
     }
 
@@ -80,14 +93,19 @@ public class Day24ImmuneSystemSimulator20XX {
             return true;
         }
 
-        Group getDefender(Group attacker) {
-            Battle battle = battles.stream().filter(b -> b.attacker == attacker).findFirst().get();
-            return battle.defender;
+        Optional<Group> getDefender(Group attacker) {
+            Optional<Battle> battle = battles.stream().filter(b -> b.attacker == attacker).findFirst();
+            return battle.map(d -> d.defender);
+        }
+
+        Stream<Battle> stream() {
+            return battles.stream();
         }
     }
 
 
-    private Set<Group> groups = new HashSet<>();
+//    private Set<Group> groups = new HashSet<>();
+    private List<Group> groups = new ArrayList<>();
 
     public Day24ImmuneSystemSimulator20XX(String fileName) throws IOException {
         readData(fileName);
@@ -96,42 +114,43 @@ public class Day24ImmuneSystemSimulator20XX {
     private void readData(String fileName) throws IOException {
         List<String> inputStrings = Files.readAllLines(Paths.get(fileName));
 
-        Pattern pattern = Pattern.compile("^(\\d+) units each with (\\d+) hit points \\((.*)\\) with an attack that does (\\d+) (\\w+) damage at initiative (\\d+)$");
+        Pattern pattern = Pattern.compile("^(?<units>\\d+) units each with (?<hitPoints>\\d+) hit points (?<modifiers>\\((.*)\\) )?with an attack that does (?<damage>\\d+) (?<attackType>\\w+) damage at initiative (?<initiative>\\d+)$");
         Pattern weaknessPattern = Pattern.compile("weak to ([\\w, ]*)");
         Pattern immunePattern = Pattern.compile("immune to ([\\w, ]*)");
         Side activeSide = null;
         int numberOfSystems = 0;
-        String name = null;
 
         for (String row : inputStrings) {
             if (row.equals("Immune System:")) {
-                activeSide = Side.immuneSystem;
+                activeSide = Side.IMMUNE_SYSTEM;
                 numberOfSystems = 0;
             } else if (row.equals("Infection:")) {
-                activeSide = Side.infection;
+                activeSide = Side.INFECTION;
                 numberOfSystems = 0;
             } else {
                 Matcher matcher = pattern.matcher(row);
                 if (matcher.find()) {
-                    int units = Integer.parseInt(matcher.group(1));
-                    int hitPoints = Integer.parseInt(matcher.group(2));
-                    String modifiers = matcher.group(3);
+                    int units = Integer.parseInt(matcher.group("units"));
+                    int hitPoints = Integer.parseInt(matcher.group("hitPoints"));
 
                     Set<String> weaknesses = new HashSet<>();
-                    Matcher weaknessMatcher = weaknessPattern.matcher(modifiers);
-                    if (weaknessMatcher.find()) {
-                        weaknesses.addAll(Arrays.asList(weaknessMatcher.group(1).trim().split("\\s*,\\s*")));
-                    }
-
                     Set<String> immunities = new HashSet<>();
-                    Matcher immuneMatcher = immunePattern.matcher(modifiers);
-                    if (immuneMatcher.find()) {
-                        immunities.addAll(Arrays.asList(immuneMatcher.group(1).trim().split("\\s*,\\s*")));
+                    String modifiers = matcher.group("modifiers");
+                    if (modifiers != null) {
+                        Matcher weaknessMatcher = weaknessPattern.matcher(modifiers);
+                        if (weaknessMatcher.find()) {
+                            weaknesses.addAll(Arrays.asList(weaknessMatcher.group(1).trim().split("\\s*,\\s*")));
+                        }
+
+                        Matcher immuneMatcher = immunePattern.matcher(modifiers);
+                        if (immuneMatcher.find()) {
+                            immunities.addAll(Arrays.asList(immuneMatcher.group(1).trim().split("\\s*,\\s*")));
+                        }
                     }
 
-                    int damage = Integer.parseInt(matcher.group(4));
-                    String attackType = matcher.group(5);
-                    int initiative = Integer.parseInt(matcher.group(6));
+                    int damage = Integer.parseInt(matcher.group("damage"));
+                    String attackType = matcher.group("attackType");
+                    int initiative = Integer.parseInt(matcher.group("initiative"));
 
                     numberOfSystems++;
                     groups.add(new Group(numberOfSystems, activeSide, units, hitPoints, initiative, damage, attackType, weaknesses, immunities));
@@ -141,14 +160,18 @@ public class Day24ImmuneSystemSimulator20XX {
     }
 
     int winningArmyUnits() {
+        int round = 1;
         do {
+            System.out.printf("Round: %d\n----------------------------\n", round);
             OrderOfBattle oob = targetSelection();
             attack(oob);
-            // Remove dead units - does not work...
-            //groups.removeIf(g -> g.units == 0);
 
-        } while (groups.stream().filter(g -> g.side == Side.immuneSystem).mapToInt(Group::getUnits).sum() > 0 &&
-                groups.stream().filter(g -> g.side == Side.infection).mapToInt(Group::getUnits).sum() > 0);
+            // Remove dead units
+            groups.removeIf(g -> g.units == 0);
+
+            round++;
+        } while (groups.stream().filter(g -> g.side == Side.IMMUNE_SYSTEM).mapToInt(Group::getUnits).sum() > 0 &&
+                groups.stream().filter(g -> g.side == Side.INFECTION).mapToInt(Group::getUnits).sum() > 0);
 
         return groups.stream().mapToInt(Group::getUnits).sum();
     }
@@ -157,50 +180,66 @@ public class Day24ImmuneSystemSimulator20XX {
         OrderOfBattle orderOfBattle = new OrderOfBattle();
 
         // Print stats
-        System.out.println("Immune System:");
-        groups.stream().filter(g -> g.getSide() == Side.immuneSystem).sorted(Comparator.comparing(Group::getId)).forEach(g -> {
-            System.out.println("Group " + g.id + " contains " + g.units + " units");
-        });
-        System.out.println("Infection:");
-        groups.stream().filter(g -> g.getSide() == Side.infection).sorted(Comparator.comparing(Group::getId)).forEach(g -> {
-            System.out.println("Group " + g.id + " contains " + g.units + " units");
-        });
+        for (Side side : Side.values()) {
+            System.out.println(side + ":");
+            groups.stream().filter(g -> g.getSide() == side)
+                    .sorted(Comparator.comparing(Group::getId))
+                    .forEach(g -> System.out.println("Group " + g.id + " contains " + g.units + " units"));
+        }
         System.out.println();
 
+        // Choose attacker in descending order of effective power, then initiative
         groups.stream().sorted(Comparator
-                .comparing(Group::effectivePower).reversed()
-                .thenComparing(Group::getInitiative))
+                .comparing(Group::effectivePower)
+                .thenComparing(Group::getInitiative).reversed())
                 .forEach(attacker -> {
-                    //System.out.println("EP: " + attacker.effectivePower() + " Group: " + attacker);
+                    // System.out.println("Attacker: " + attacker + ", EP: " + attacker.effectivePower());
                     // Chose enemy to attack. Should not have been targeted for attack earlier.
                     // order by most damage, then largest effective power, then highest initiative
-                    Optional<Group> defender = groups.stream().filter(g -> g.getSide() != attacker.getSide())
-                            .filter(orderOfBattle::isUnattacked).max(Comparator.comparing(attacker::computeDamage)
+                    Optional<Group> defender = groups.stream()
+                            .filter(g -> g.getSide() != attacker.getSide())
+                            .filter(orderOfBattle::isUnattacked)
+                            .max(Comparator
+                                    .comparing(attacker::computeDamage)
                                     .thenComparing(Group::effectivePower)
                                     .thenComparing(Group::getInitiative));
                     defender.ifPresent(d -> {
-                        orderOfBattle.addAttack(new Battle(attacker, d));
-                        System.out.println(attacker.side + " group " + attacker.id + " would deal defending group " + d.id + " " + attacker.computeDamage(d) + " damage");
+                        // "If it cannot deal any defending groups damage, it does not choose a target."
+                        if (attacker.computeDamage(d) > 0) {
+                            orderOfBattle.addAttack(new Battle(attacker, d));
+                            //System.out.println("## " + attacker.side + " group " + attacker.id + " would deal defending group " + d.id + " " + attacker.computeDamage(d) + " damage");
+                        }
                     });
 
 //                    for (Group d : groups) {
-//                        System.out.println("---Damage: " + attacker.computeDamage(d) + ", defender: " + d);
+//                        if (d.side != attacker.side) {
+//                            System.out.println("---Damage: " + attacker.computeDamage(d) + ", defender: " + d + ", EP: " + d.effectivePower() + ", Unattacked: " + orderOfBattle.isUnattacked(d));
+//                        }
 //                    }
                 });
+
+        for (Side side : Side.values()) {
+            orderOfBattle.stream()
+                    .filter(b -> b.attacker.side == side)
+                    .sorted(Comparator.comparing(b -> b.attacker.getId()))
+                    .forEach(b -> System.out.println(b.attacker.side + " group " + b.attacker.id + " would deal defending group " + b.defender.id + " " + b.attacker.computeDamage(b.defender) + " damage"));
+        }
         System.out.println();
 
         return orderOfBattle;
     }
 
     private void attack(OrderOfBattle orderOfBattle) {
+        // Groups attack in decreasing order of initiative
         groups.stream().sorted(Comparator.comparing(Group::getInitiative).reversed()).forEach(attacker -> {
-            Group defender = orderOfBattle.getDefender(attacker);
-            int damage = attacker.computeDamage(defender);
-//            System.out.println("Attacker: " + attacker + ", defender: " + defender);
-            int killedUnits = defender.takeDamage(damage);
-//            System.out.println("After the attack, defender took " + damage + " damage: " + defender);
-            System.out.println(attacker.side + " group " + attacker.id + " attacks defending group " + defender.id + ", killing " + killedUnits + " units");
+            Optional<Group> defender = orderOfBattle.getDefender(attacker);
+            defender.ifPresent(def -> {
+                int damage = attacker.computeDamage(def);
+                int killedUnits = def.takeDamage(damage);
+                System.out.println(attacker.side + " group " + attacker.id + " attacks defending group " + def.id + ", killing " + killedUnits + " units");
+            });
         });
+        System.out.println();
     }
 
 }
--- src/test/java/Day24ImmuneSystemSimulator20XXTest.java
+++ src/test/java/Day24ImmuneSystemSimulator20XXTest.java
@@ -1,16 +1,15 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day24ImmuneSystemSimulator20XXTest {
 
     @ParameterizedTest
     @CsvSource({"5216, out/test/resources/day24-demo0.txt",
-            "0, out/test/resources/day24.txt"})
+            "15493, out/test/resources/day24.txt"})
     void winningArmyUnits(int expected, String fileName) throws IOException {
         assertEquals(expected, new Day24ImmuneSystemSimulator20XX(fileName).winningArmyUnits());
     }
