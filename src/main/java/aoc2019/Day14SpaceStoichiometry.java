d2f05bb79777cc9ff475c4c788c3e664b08270b4
martin67 <martin@hagelin.nu>
2019-12-16 00:45:03 +0100

    Day 14

--- src/main/java/aoc2019/Day14SpaceStoichiometry.java
+++ src/main/java/aoc2019/Day14SpaceStoichiometry.java
@@ -1,4 +1,65 @@
 package aoc2019;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
 public class Day14SpaceStoichiometry {
+
+    @Data
+    @AllArgsConstructor
+    class Chemical {
+        String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class ChemicalQuantity {
+        Chemical chemical;
+        int amount;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class Reaction {
+        Set<ChemicalQuantity> inputs;
+        ChemicalQuantity output;
+    }
+
+    Set<Chemical> chemicals = new HashSet<>();
+    Set<Reaction> reactions = new HashSet<>();
+
+    public Day14SpaceStoichiometry(List<String> inputLines) {
+        Pattern pattern = Pattern.compile("^(((\\d+) (\\w+),? )+)=> (?<outputAmount>\\d+) (?<outputChemical>\\w+)$");
+        for (String line : inputLines) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                Set<ChemicalQuantity> inputs = new HashSet<>();
+                String indata = matcher.group(1);
+                for (String quantity : indata.split(",")) {
+                    String[] split = quantity.trim().split(" ");
+                    Chemical inputChemical = new Chemical(split[1]);
+                    chemicals.add(inputChemical);
+                    ChemicalQuantity inputQuantity = new ChemicalQuantity(inputChemical, Integer.parseInt(split[0]));
+                    inputs.add(inputQuantity);
+                }
+                Chemical outputChemical = new Chemical(matcher.group("outputChemical"));
+                chemicals.add(outputChemical);
+                ChemicalQuantity output = new ChemicalQuantity(outputChemical, Integer.parseInt(matcher.group("outputAmount")));
+                reactions.add(new Reaction(inputs, output));
+            }
+        }
+    }
+
+    int minimumAmountOfOre() {
+        log.info("C: {}", reactions);
+        return 0;
+    }
 }
--- src/test/java/aoc2019/Day14SpaceStoichiometryTest.java
+++ src/test/java/aoc2019/Day14SpaceStoichiometryTest.java
@@ -1,7 +1,25 @@
 package aoc2019;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day14SpaceStoichiometryTest {
-
+    @ParameterizedTest
+    @CsvSource({"31, src/test/resources/2019/day14-demo1.txt",
+            "165, src/test/resources/2019/day14-demo2.txt",
+            "13312, src/test/resources/2019/day14-demo3.txt",
+            "180697, src/test/resources/2019/day14-demo4.txt",
+            "2210736, src/test/resources/2019/day14-demo5.txt",
+            "0, src/test/resources/2019/day14.txt"})
+    void minimumAmountOfOre(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day14SpaceStoichiometry(inputLines).minimumAmountOfOre());
+    }
 }
