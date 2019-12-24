a42ab20e0875893b5ee4bdd983a9f4aa703c78c0
Martin Hagelin <martin@hagelin.nu>
2019-12-24 22:09:27 +0100

    Day 24 part 1

---
+++ src/test/java/aoc2019/Day24PlanetOfDiscordTest.java
@@ -0,0 +1,24 @@
+package aoc2019;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day24PlanetOfDiscordTest {
+
+    @ParameterizedTest
+    @CsvSource({"2129920, src/test/resources/2019/day24-demo1.txt",
+            "28778811, src/test/resources/2019/day24.txt"})
+    void biodiversityRating(int expected, String fileName) throws IOException, InterruptedException, ExecutionException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day24PlanetOfDiscord(inputLines).biodiversityRating());
+    }
+}
