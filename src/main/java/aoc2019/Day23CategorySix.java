004cbad94ca728ee968104988639d2226cc8e346
Martin Hagelin <martin@hagelin.nu>
2019-12-24 15:30:57 +0100

    New days

--- src/main/java/aoc2019/Day23CategorySix.java
+++ src/main/java/aoc2019/Day23CategorySix.java
@@ -1,4 +1,76 @@
 package aoc2019;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
 public class Day23CategorySix {
+
+    @Data
+    class Router implements Callable<Integer> {
+        BlockingQueue<BigInteger> inputQueue;
+        BlockingQueue<BigInteger> outputQueue;
+        int id;
+
+        public Router(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            log.info("Starting router {}", id);
+            while (true) {
+                int destination = inputQueue.take().intValue();
+                BigInteger x = inputQueue.take();
+                BigInteger y = inputQueue.take();
+                log.info("Router {}: Routing {},{} to {}", id, x.intValue(), y.intValue(), destination);
+                computers.get(destination).getInputQueue().put(x);
+                computers.get(destination).getInputQueue().put(y);
+            }
+        }
+    }
+
+    ExecutorService executorService;
+    private final List<String> opcodes;
+    List<IntcodeComputer> computers = new ArrayList<>();
+    List<Router> routers = new ArrayList<>();
+
+    public Day23CategorySix(List<String> inputLines) {
+        executorService = Executors.newCachedThreadPool();
+        opcodes = Stream.of(inputLines.get(0).split(","))
+                .collect(Collectors.toList());
+    }
+
+    int yValue() throws InterruptedException {
+        log.info("Starting...");
+        for (int i = 0; i < 50; i++) {
+            IntcodeComputer ic = new IntcodeComputer(new ArrayList<>(opcodes));
+            Router router = new Router(i);
+            ic.getInputQueue().add(new BigInteger(String.valueOf(i)));
+            router.setInputQueue(ic.getOutputQueue());
+            computers.add(ic);
+            routers.add(router);
+            executorService.submit(router);
+            executorService.submit(ic);
+        }
+
+
+
+
+        //executorService.invokeAll(routers);
+        //executorService.invokeAll(computers);
+
+        log.info("Done");
+        return 0;
+    }
 }
--- src/test/java/aoc2019/Day23CategorySixTest.java
+++ src/test/java/aoc2019/Day23CategorySixTest.java
@@ -1,7 +1,19 @@
 package aoc2019;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day23CategorySixTest {
-
+    @Test
+    void yValue() throws IOException, InterruptedException {
+        List<String> inputLines = Files.readAllLines(Paths.get("src/test/resources/2019/day23.txt"));
+        assertEquals(0, new Day23CategorySix(inputLines).yValue());
+    }
 }
