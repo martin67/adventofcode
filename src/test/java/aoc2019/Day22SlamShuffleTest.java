33bc50bf8b50c9b2e53d40719d6fac03a36a249e
Martin Hagelin <martin@hagelin.nu>
2019-12-25 04:50:58 +0100

    Day 22 part 1

--- src/test/java/aoc2019/Day22SlamShuffleTest.java
+++ src/test/java/aoc2019/Day22SlamShuffleTest.java
@@ -1,7 +1,24 @@
 package aoc2019;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day22SlamShuffleTest {
-
+    @ParameterizedTest
+    @CsvSource({"0, 10, src/test/resources/2019/day22-demo1.txt",
+            "0, 10, src/test/resources/2019/day22-demo2.txt",
+            "0, 10, src/test/resources/2019/day22-demo3.txt",
+            "0, 10, src/test/resources/2019/day22-demo4.txt",
+            "4086, 10007, src/test/resources/2019/day22.txt"})
+    void cardPosition(int expected, int numberOfCards, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day22SlamShuffle(inputLines).cardPosition(numberOfCards));
+    }
 }
