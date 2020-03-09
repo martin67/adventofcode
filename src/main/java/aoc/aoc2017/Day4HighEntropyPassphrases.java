d7e509f4587d4ccdb62fbcd93862bf3cae5237e8
martin67 <martin@hagelin.nu>
2020-03-09 04:10:16 +0100

    Adding old stuff

--- src/main/java/aoc/aoc2017/Day4HighEntropyPassphrases.java
+++ src/main/java/aoc/aoc2017/Day4HighEntropyPassphrases.java
@@ -1,29 +1,39 @@
 package aoc.aoc2017;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Day4HighEntropyPassphrases {
     boolean validPassphrase(String input) {
         Set<String> phrases = new HashSet<>();
-        for (String s : input.split("\\s")) {
-            if (phrases.contains(s)) {
+        for (String phrase : input.split("\\s")) {
+            if (phrases.contains(phrase)) {
                 return false;
             }
-            phrases.add(s);
+            phrases.add(phrase);
         }
         return true;
     }
 
-    int numberOfValidPassphrases(String input) {
+    boolean validAnagramPassphrase(String input) {
         Set<String> phrases = new HashSet<>();
-        for (String s : input.split("\\s")) {
-            if (phrases.contains(s)) {
+        for (String phrase : input.split("\\s")) {
+            if (phrases.contains(phrase)) {
                 return false;
             }
-            phrases.add(s);
+            phrases.add(phrase);
         }
         return true;
     }
+
+    int numberOfValidPassphrases(List<String> input) {
+        int numberOfValidPassphrases = 0;
+        for (String line : input) {
+            if (validPassphrase(line)) {
+                numberOfValidPassphrases++;
+            }
+        }
+        return numberOfValidPassphrases;
     }
 }
--- src/test/java/aoc/aoc2017/Day4HighEntropyPassphrasesTest.java
+++ src/test/java/aoc/aoc2017/Day4HighEntropyPassphrasesTest.java
@@ -23,8 +23,17 @@ class Day4HighEntropyPassphrasesTest {
     @Test
     void numberOfValidPassphrases() throws IOException {
         List<String> inputLines = Files.readAllLines(Paths.get("src/test/resources/2017/day4.txt"));
-        for (String line : inputLines) {
-            assertEquals(995, new Day1InverseCaptcha().computeCaptcha(line));
-        }
+        assertEquals(451, new Day4HighEntropyPassphrases().numberOfValidPassphrases(inputLines));
     }
+
+    @ParameterizedTest
+    @CsvSource({"true, abcde fghij",
+            "false, abcde xyz ecdab",
+            "true, a ab abc abd abf abj",
+            "true, iiii oiii ooii oooi oooo",
+            "false, oiii ioii iioi iiio"})
+    void validAnagramPasshrase(boolean valid, String passphrase) {
+        assertEquals(valid, new Day4HighEntropyPassphrases().validAnagramPassphrase(passphrase));
+    }
+
 }
