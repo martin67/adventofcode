163304ae5a1924b4671951fd25f9330af62efdf8
Martin Hagelin <martin@hagelin.nu>
2020-06-15 12:42:40 +0200

    2015 day 12 part 2 complete

--- src/main/java/aoc/aoc2015/Day12JSAbacusFramework.java
+++ src/main/java/aoc/aoc2015/Day12JSAbacusFramework.java
@@ -1,13 +1,21 @@
 package aoc.aoc2015;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class Day12JSAbacusFramework {
-    int sumOfAllNumbers = 0;
+    List<String> inputLines;
 
     public Day12JSAbacusFramework(List<String> inputLines) {
+        this.inputLines = inputLines;
+    }
+
+    int sumOfAllNumbers() {
+        int sumOfAllNumbers = 0;
         Pattern pattern = Pattern.compile("(-?\\d+)");
         for (String line : inputLines) {
             Matcher matcher = pattern.matcher(line);
@@ -15,9 +23,98 @@ public class Day12JSAbacusFramework {
                 sumOfAllNumbers += Integer.parseInt(matcher.group(1));
             }
         }
+        return sumOfAllNumbers;
     }
 
-    int sumOfAllNumbers() {
-        return sumOfAllNumbers;
+    int sumOfAllNumbersIgnoringRed() {
+        Result result = null;
+        switch (inputLines.get(0).charAt(0)) {
+            case '[':
+                result = parseArray(inputLines.get(0).substring(1));
+                break;
+            case '{':
+                result = parseObject(inputLines.get(0).substring(1));
+                break;
+            default:
+                assert false;
+                break;
+        }
+        return result.sum;
+    }
+
+
+    Result parseArray(String in) {
+        Result result = new Result();
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        while (in.charAt(index) != ']') {
+            Result r;
+            switch (in.charAt(index)) {
+                case '[':
+                    r = parseArray(in.substring(index + 1));
+                    index += r.index;
+                    result.sum += r.sum;
+                    break;
+                case '{':
+                    r = parseObject(in.substring(index + 1));
+                    index += r.index;
+                    result.sum += r.sum;
+                    break;
+                default:
+                    sb.append(in.charAt(index));
+                    index++;
+                    break;
+            }
+        }
+        Pattern pattern = Pattern.compile("(-?\\d+)");
+        Matcher matcher = pattern.matcher(sb);
+        while (matcher.find()) {
+            result.sum += Integer.parseInt(matcher.group(1));
+        }
+        log.debug("Array sum: {}, string: {}", result.sum, sb);
+        result.index = index + 2;
+        return result;
+    }
+
+    Result parseObject(String in) {
+        Result result = new Result();
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        while (in.charAt(index) != '}') {
+            Result r;
+            switch (in.charAt(index)) {
+                case '[':
+                    r = parseArray(in.substring(index + 1));
+                    index += r.index;
+                    result.sum += r.sum;
+                    break;
+                case '{':
+                    r = parseObject(in.substring(index + 1));
+                    index += r.index;
+                    result.sum += r.sum;
+                    break;
+                default:
+                    sb.append(in.charAt(index));
+                    index++;
+                    break;
+            }
+        }
+        if (sb.toString().contains("red")) {
+            result.sum = 0;
+        } else {
+            Pattern pattern = Pattern.compile("(-?\\d+)");
+            Matcher matcher = pattern.matcher(sb);
+            while (matcher.find()) {
+                result.sum += Integer.parseInt(matcher.group(1));
+            }
+        }
+        log.debug("Object sum: {}, string: {}", result.sum, sb);
+        result.index = index + 2;
+        return result;
+    }
+
+    static class Result {
+        int index;
+        int sum;
     }
 }
--- src/test/java/aoc/aoc2015/Day12JSAbacusFrameworkTest.java
+++ src/test/java/aoc/aoc2015/Day12JSAbacusFrameworkTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,4 +20,21 @@ class Day12JSAbacusFrameworkTest {
         assertEquals(expected, new Day12JSAbacusFramework(inputLines).sumOfAllNumbers());
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"6; [1,2,3]",
+            "4; [1,{\"c\":\"red\",\"b\":2},3]",
+            "0; {\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}",
+            "6; [1,\"red\",5]",
+            "0; []",
+            "0; {}"}, delimiter = ';')
+    void ignoringRed(int expected, String in) {
+        assertEquals(expected, new Day12JSAbacusFramework(Collections.singletonList(in)).sumOfAllNumbersIgnoringRed());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"87842, src/test/resources/2015/day12.txt"})
+    void sumOfAllNumbersIgnoringRed(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day12JSAbacusFramework(inputLines).sumOfAllNumbersIgnoringRed());
+    }
 }
