9055d736f3a81715e4efed594a7077cc7c4722f5
martin67 <martin@hagelin.nu>
2020-12-02 08:25:58 +0100

    2020, day 2

---
+++ src/main/java/aoc/aoc2020/Day2PasswordPhilosophy.java
@@ -0,0 +1,63 @@
+package aoc.aoc2020;
+
+import com.google.common.base.CharMatcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day2PasswordPhilosophy {
+    List<PasswordAndPolicy> passwordAndPolicies = new ArrayList<>();
+
+    public Day2PasswordPhilosophy(List<String> inputLines) {
+        Pattern pattern = Pattern.compile("^(\\d+)-(\\d+) (\\w): (\\w+)$");
+
+        for (String line : inputLines) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                passwordAndPolicies.add(new PasswordAndPolicy(
+                        Integer.parseInt(matcher.group(1)),
+                        Integer.parseInt(matcher.group(2)),
+                        matcher.group(3).charAt(0),
+                        matcher.group(4))
+                );
+            }
+        }
+    }
+
+    long validPasswords() {
+        return passwordAndPolicies.stream().filter(PasswordAndPolicy::validPassword).count();
+    }
+
+    long validPasswordsNewPolicy() {
+        return passwordAndPolicies.stream().filter(PasswordAndPolicy::validPasswordNewPolicy).count();
+    }
+
+    static class PasswordAndPolicy {
+        int min;
+        int max;
+        char letter;
+        String password;
+
+        public PasswordAndPolicy(int min, int max, char letter, String password) {
+            this.min = min;
+            this.max = max;
+            this.letter = letter;
+            this.password = password;
+        }
+
+        boolean validPassword() {
+            int frequency = CharMatcher.is(letter).countIn(password);
+
+            return (frequency >= min && frequency <= max);
+        }
+
+        boolean validPasswordNewPolicy() {
+            boolean firstPos = (password.charAt(min - 1) == letter);
+            boolean secondPos = (password.charAt(max - 1) == letter);
+
+            return ((firstPos && !secondPos) || (!firstPos && secondPos));
+        }
+    }
+}
---
+++ src/test/java/aoc/aoc2020/Day2PasswordPhilosophyTest.java
@@ -0,0 +1,31 @@
+package aoc.aoc2020;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day2PasswordPhilosophyTest {
+
+    @ParameterizedTest
+    @CsvSource({"2, src/test/resources/2020/day2-demo1.txt",
+            "603, src/test/resources/2020/day2.txt"})
+    void validPasswords(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day2PasswordPhilosophy(inputLines).validPasswords());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, src/test/resources/2020/day2-demo1.txt",
+            "404, src/test/resources/2020/day2.txt"})
+    void validPasswordsNewPolicy(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day2PasswordPhilosophy(inputLines).validPasswordsNewPolicy());
+    }
+
+}
