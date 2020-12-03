4c9da6ef912d1a3daf8d2272aeedafc65a17fefd
martin67 <martin@hagelin.nu>
2020-12-03 11:53:18 +0100

    2020, day 3

---
+++ src/main/java/aoc/aoc2020/Day3TobogganTrajectory.java
@@ -0,0 +1,62 @@
+package aoc.aoc2020;
+
+import aoc.Position;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day3TobogganTrajectory {
+    Set<Position> trees = new HashSet<>();
+    int width;
+    int height;
+
+    public Day3TobogganTrajectory(List<String> inputLines) {
+        int x = 0;
+        int y = 0;
+
+        for (String line : inputLines) {
+            x = 0;
+            for (char c : line.toCharArray()) {
+                if (c == '#') {
+                    trees.add(new Position(x, y));
+                }
+                x++;
+            }
+            y++;
+        }
+        width = x;
+        height = y;
+    }
+
+    int treesEncountered(int right, int down) {
+        Position position = new Position(0, 0);
+        int numberOfTrees = 0;
+
+        while (position.getY() < height) {
+            if (trees.contains(position)) {
+                numberOfTrees++;
+            }
+            position.setY(position.getY() + down);
+            position.setX(position.getX() + right);
+            if (position.getX() >= width) {
+                position.setX(position.getX() - width);
+            }
+        }
+
+        return numberOfTrees;
+    }
+
+    long treesSecondEncounter() {
+        long numberOfTrees;
+
+        numberOfTrees = treesEncountered(1, 1);
+        numberOfTrees *= treesEncountered(3, 1);
+        numberOfTrees *= treesEncountered(5, 1);
+        numberOfTrees *= treesEncountered(7, 1);
+        numberOfTrees *= treesEncountered(1, 2);
+
+        return numberOfTrees;
+    }
+}
+
---
+++ src/test/java/aoc/aoc2020/Day3TobogganTrajectoryTest.java
@@ -0,0 +1,31 @@
+package aoc.aoc2020;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day3TobogganTrajectoryTest {
+
+    @ParameterizedTest
+    @CsvSource({"7, src/test/resources/2020/day3-demo1.txt",
+            "176, src/test/resources/2020/day3.txt"})
+    void treesEncountered(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day3TobogganTrajectory(inputLines).treesEncountered(3, 1));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"336, src/test/resources/2020/day3-demo1.txt",
+            "5872458240, src/test/resources/2020/day3.txt"})
+    void treesSecondEncounter(long expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day3TobogganTrajectory(inputLines).treesSecondEncounter());
+    }
+
+}
