a1edc96b9504aa14221fd826cd82d9ebe5fb26e6
martin67 <martin@hagelin.nu>
2020-12-08 06:49:16 +0100

    2020, day 8

---
+++ src/main/java/aoc/aoc2020/Day8HandheldHalting.java
@@ -0,0 +1,102 @@
+package aoc.aoc2020;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Day8HandheldHalting {
+    List<Instruction> instructions = new ArrayList<>();
+
+    public Day8HandheldHalting(List<String> inputLines) {
+        for (String line : inputLines) {
+            String[] s = line.split(" ");
+            String operation = s[0];
+            int argument = Integer.parseInt(s[1]);
+            instructions.add(new Instruction(operation, argument));
+        }
+    }
+
+    int accumulatorValue() {
+        return runProgram(instructions).value;
+    }
+
+    Result runProgram(List<Instruction> program) {
+        int accumulator = 0;
+        int pc = 0;
+        Set<Integer> instructionsRun = new HashSet<>();
+
+        while (!instructionsRun.contains(pc) && pc != program.size()) {
+            instructionsRun.add(pc);
+            switch (program.get(pc).operation) {
+                case "nop":
+                    pc++;
+                    break;
+
+                case "acc":
+                    accumulator += program.get(pc).argument;
+                    pc++;
+                    break;
+
+                case "jmp":
+                    pc += program.get(pc).argument;
+                    break;
+
+                default:
+                    assert false : "Ooops";
+            }
+        }
+
+        if (pc == program.size()) {
+            return new Result(false, accumulator);
+        } else {
+            return new Result(true, accumulator);
+        }
+    }
+
+    int accumulatorValueNoLoop() {
+        int instructionToChange = 0;
+        List<Instruction> modifiedInstructions;
+        Result result;
+        do {
+            modifiedInstructions = new ArrayList<>();
+            for (Instruction instruction : instructions) {
+                modifiedInstructions.add(new Instruction(instruction.operation, instruction.argument));
+            }
+
+            while (modifiedInstructions.get(instructionToChange).operation.equals("acc")) {
+                instructionToChange++;
+            }
+
+            if (modifiedInstructions.get(instructionToChange).operation.equals("jmp")) {
+                modifiedInstructions.get(instructionToChange).operation = "nop";
+            } else {
+                modifiedInstructions.get(instructionToChange).operation = "jmp";
+            }
+
+            result = runProgram(modifiedInstructions);
+            instructionToChange++;
+        } while (result.looped);
+        return result.value;
+    }
+
+    static class Instruction {
+        String operation;
+        int argument;
+
+        public Instruction(String operation, int argument) {
+            this.operation = operation;
+            this.argument = argument;
+        }
+    }
+
+    static class Result {
+        boolean looped;
+        int value;
+
+        public Result(boolean looped, int value) {
+            this.looped = looped;
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/aoc/aoc2020/Day8HandheldHaltingTest.java
@@ -0,0 +1,31 @@
+package aoc.aoc2020;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day8HandheldHaltingTest {
+
+    @ParameterizedTest
+    @CsvSource({"5, src/test/resources/2020/day8-demo1.txt",
+            "1915, src/test/resources/2020/day8.txt"})
+    void accumulatorValue(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day8HandheldHalting(inputLines).accumulatorValue());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"8, src/test/resources/2020/day8-demo1.txt",
+            "944, src/test/resources/2020/day8.txt"})
+    void accumulatorValueNoLoop(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day8HandheldHalting(inputLines).accumulatorValueNoLoop());
+    }
+
+}
