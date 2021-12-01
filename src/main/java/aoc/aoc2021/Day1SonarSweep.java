71a734417c0c1feed93016936719fe141407a6fa
Martin Hagelin <martin@hagelin.nu>
2021-12-01 09:51:52 +0100

    2021, day 1, part 1 & 2

---
+++ src/main/java/aoc/aoc2021/Day1SonarSweep.java
@@ -0,0 +1,41 @@
+package aoc.aoc2021;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1SonarSweep {
+
+    List<Integer> inputValues = new ArrayList<>();
+
+    public Day1SonarSweep(List<String> inputLines) {
+        inputLines.forEach(line -> inputValues.add(Integer.parseInt(line)));
+    }
+
+    int problem1() {
+        return countIncreases(inputValues);
+    }
+
+    int problem2() {
+        List<Integer> slidingWindow = new ArrayList<>();
+
+        for (int i = 0; i < inputValues.size(); i++) {
+            if (i > 1) {
+                slidingWindow.add(inputValues.get(i) + inputValues.get(i - 1) + inputValues.get(i - 2));
+            }
+        }
+
+        return countIncreases(slidingWindow);
+    }
+
+    private int countIncreases(List<Integer> inputValues) {
+        int previousDepth = Integer.MAX_VALUE;
+        int increases = 0;
+        for (int depth : inputValues) {
+            if (depth > previousDepth) {
+                increases++;
+            }
+            previousDepth = depth;
+        }
+        return increases;
+    }
+}
---
+++ src/test/java/aoc/aoc2021/Day1SonarSweepTest.java
@@ -0,0 +1,30 @@
+package aoc.aoc2021;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day1SonarSweepTest {
+
+    @ParameterizedTest
+    @CsvSource({"7, src/test/resources/2021/day1-demo1.txt",
+            "1298, src/test/resources/2021/day1.txt"})
+    void problem1(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day1SonarSweep(inputLines).problem1());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"5, src/test/resources/2021/day1-demo2.txt",
+            "1248, src/test/resources/2021/day1.txt"})
+    void problem2(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day1SonarSweep(inputLines).problem2());
+    }
+}
