3d07ebec171646ad25e51d9bd3aa98bb18b549fd
martin67 <martin@hagelin.nu>
2021-12-03 08:37:32 +0100

    2021, day 3 part 1 & 2

---
+++ src/main/java/aoc/aoc2021/Day3BinaryDiagnostic.java
@@ -0,0 +1,82 @@
+package aoc.aoc2021;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3BinaryDiagnostic {
+
+    List<Integer> numbers = new ArrayList<>();
+    int width;
+
+    public Day3BinaryDiagnostic(List<String> inputLines) {
+        for (String line : inputLines) {
+            width = line.length();
+            numbers.add(Integer.parseInt(line, 2));
+        }
+    }
+
+    int problem1() {
+        int gamma = 0;
+        int epsilon = 0;
+
+        for (int position = 0; position < width; position++) {
+            int mask = 1 << width - position - 1;
+
+            int ones = 0;
+            int zeros = 0;
+            for (int i : numbers) {
+                if ((i & mask) != 0) {
+                    ones++;
+                } else {
+                    zeros++;
+                }
+            }
+            if (ones > zeros) {
+                gamma += mask;
+            } else {
+                epsilon += mask;
+            }
+        }
+        return gamma * epsilon;
+    }
+
+    int problem2() {
+
+        List<Integer> numbersCopy = new ArrayList<>(numbers);
+        for (int i = 0; i < width; i++) {
+            numbersCopy = bitCriteria(numbersCopy, i, true);
+        }
+        int oxygen = numbersCopy.get(0);
+
+        numbersCopy = new ArrayList<>(numbers);
+        for (int i = 0; i < width; i++) {
+            numbersCopy = bitCriteria(numbersCopy, i, false);
+        }
+        int co2 = numbersCopy.get(0);
+
+        return oxygen * co2;
+    }
+
+    List<Integer> bitCriteria(List<Integer> in, int position, boolean oxygen) {
+        List<Integer> zeros = new ArrayList<>();
+        List<Integer> ones = new ArrayList<>();
+
+        if (in.size() == 1) {
+            return in;
+        }
+
+        for (int i : in) {
+            if ((i & (1 << width - position - 1)) != 0) {
+                ones.add(i);
+            } else {
+                zeros.add(i);
+            }
+        }
+
+        if (zeros.size() > ones.size()) {
+            return oxygen ? zeros : ones;
+        } else {
+            return oxygen ? ones : zeros;
+        }
+    }
+}
---
+++ src/test/java/aoc/aoc2021/Day3BinaryDiagnosticTest.java
@@ -0,0 +1,30 @@
+package aoc.aoc2021;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day3BinaryDiagnosticTest {
+
+    @ParameterizedTest
+    @CsvSource({"198, src/test/resources/2021/day3-demo1.txt",
+            "3895776, src/test/resources/2021/day3.txt"})
+    void problem1(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day3BinaryDiagnostic(inputLines).problem1());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"230, src/test/resources/2021/day3-demo1.txt",
+            "7928162, src/test/resources/2021/day3.txt"})
+    void problem2(int expected, String fileName) throws IOException {
+        List<String> inputLines = Files.readAllLines(Paths.get(fileName));
+        assertEquals(expected, new Day3BinaryDiagnostic(inputLines).problem2());
+    }
+}
