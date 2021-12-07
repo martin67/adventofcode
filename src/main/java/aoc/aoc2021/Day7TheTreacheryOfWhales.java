79a177c1bf8cd708f8f21e5e7f19d95e7476af0c
Martin Hagelin <martin@hagelin.nu>
2021-12-07 06:30:12 +0100

    2021, day 7, part 1 & 2

---
+++ src/main/java/aoc/aoc2021/Day7TheTreacheryOfWhales.java
@@ -0,0 +1,56 @@
+package aoc.aoc2021;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class Day7TheTreacheryOfWhales {
+
+    List<Integer> crabs = new ArrayList<>();
+    int min;
+    int max;
+
+    public Day7TheTreacheryOfWhales(List<String> inputLines) {
+        inputLines.forEach(line -> crabs = Stream.of(line.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList()));
+        min = crabs.stream().mapToInt(v -> v)
+                .min().orElseThrow(NoSuchElementException::new);
+        max = crabs.stream().mapToInt(v -> v)
+                .max().orElseThrow(NoSuchElementException::new);
+    }
+
+    int problem1() {
+        int minFuel = Integer.MAX_VALUE;
+        for (int i = min; i < max; i++) {
+            int fuel = 0;
+            for (int crab : crabs) {
+                fuel += Math.abs(crab - i);
+            }
+            if (fuel < minFuel) {
+                minFuel = fuel;
+            }
+        }
+        return minFuel;
+    }
+
+    int problem2() {
+        int minFuel = Integer.MAX_VALUE;
+        for (int i = min; i < max; i++) {
+            int fuel = 0;
+            for (int crab : crabs) {
+                int n = Math.abs(crab - i);
+                fuel += n * (n + 1) / 2;
+            }
+            if (fuel < minFuel) {
+                minFuel = fuel;
+            }
+        }
+        return minFuel;
+    }
+}
