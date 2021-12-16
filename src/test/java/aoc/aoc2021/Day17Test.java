bf9258932c7b8f5efeebfeb3ce702cfcc59183f0
martin67 <martin@hagelin.nu>
2021-12-16 17:23:25 +0100

    2021, cleanup

--- src/test/java/aoc/aoc2021/Day17Test.java
+++ src/test/java/aoc/aoc2021/Day17Test.java
@@ -11,23 +11,23 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("2021: Day 13: Transparent Origami")
-class Day13TransparentOrigamiTest {
+@DisplayName("2021: Day 17: ")
+class Day17Test {
 
     @ParameterizedTest
-    @CsvSource({"17, src/test/resources/2021/day13-demo1.txt",
-            "775, src/test/resources/2021/day13.txt"})
+    @CsvSource({"0, src/test/resources/2021/day17-demo1.txt",
+            "0, src/test/resources/2021/day17.txt"})
     void problem1(int expected, String fileName) throws IOException {
         List<String> inputLines = Files.readAllLines(Paths.get(fileName));
-        assertEquals(expected, new Day13TransparentOrigami(inputLines).problem1());
+        assertEquals(expected, new Day17(inputLines).problem1());
     }
 
     @ParameterizedTest
-    @CsvSource({"0, src/test/resources/2021/day13-demo1.txt",
-            "0, src/test/resources/2021/day13.txt"})
+    @CsvSource({"0, src/test/resources/2021/day17-demo1.txt",
+            "0, src/test/resources/2021/day17.txt"})
     void problem2(int expected, String fileName) throws IOException {
         List<String> inputLines = Files.readAllLines(Paths.get(fileName));
-        assertEquals(expected, new Day13TransparentOrigami(inputLines).problem2());
+        assertEquals(expected, new Day17(inputLines).problem2());
     }
 
 }
