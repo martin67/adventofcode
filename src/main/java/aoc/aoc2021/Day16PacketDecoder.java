e0b35627224937a562b969a9526e33c24a0f55b4
Martin Hagelin <martin@hagelin.nu>
2021-12-16 17:05:46 +0100

    2021, day 16, part 2

--- src/main/java/aoc/aoc2021/Day16PacketDecoder.java
+++ src/main/java/aoc/aoc2021/Day16PacketDecoder.java
@@ -20,74 +20,210 @@ public class Day16PacketDecoder {
         binary = sb.toString();
     }
 
-    String decodePacket(String binaryInput) {
+    Result decodePacket(String binaryInput) {
+        Result result = new Result();
+        char lengthTypeId;
+        int length;
         int pos = 0;
+
         while (pos < binaryInput.length()) {
-            int version = Integer.parseInt(binaryInput.substring(pos, pos + 3), 2);
-            pos += 3;
+            int version = Integer.parseInt(binaryInput.substring(pos, pos += 3), 2);
             versionNumbers += version;
-            int id = Integer.parseInt(binaryInput.substring(pos, pos + 3), 2);
-            pos += 3;
+            int id = Integer.parseInt(binaryInput.substring(pos, pos += 3), 2);
 
-            if (id == 4) {
-                log.info("Decoding literal, version {}, id: {}", version, id);
-                // Literal value
-                StringBuilder number = new StringBuilder();
-                //int n = Integer.parseInt(binary.substring(pos, pos + 5), 2);
-                boolean stop = false;
-                do {
-                    String n = binaryInput.substring(pos, pos + 5);
-                    number.append(n.substring(1));
-                    if (n.charAt(0) == '0') {
-                        stop = true;
-                    }
-                    pos += 5;
-                } while (!stop);
-                int n2 = Integer.parseInt(number.toString(), 2);
-                log.info("number: {}", n2);
-                // pad if needed
-                log.info("padding pos {} with {} => pos = {}", pos, pos % 4, pos + pos % 4);
-                pos += pos % 4;
-                return binaryInput.substring(pos);
-            } else {
-                // operator
-                char lengthTypeId = binaryInput.charAt(pos);
-                pos++;
-                log.info("Decoding operator, version {}, id: {}, type: {}", version, id, lengthTypeId);
-
-                int length;
-                String out;
-                if (lengthTypeId == '0') {
-                    length = Integer.parseInt(binaryInput.substring(pos, pos + 15), 2);
-                    log.info("length: {}", length);
-                    pos += 15;
-                    String t = binaryInput.substring(pos, length - 1);
+            switch (id) {
+                case 4:
+                    log.debug("Decoding literal, version {}, id: {}, in length: {}", version, id, binaryInput.length());
+                    StringBuilder number = new StringBuilder();
+                    boolean stop = false;
                     do {
-                        out = decodePacket(binaryInput.substring(pos, pos + length - 1));
-                        log.info("After {}, {}", out, out.length());
+                        String n = binaryInput.substring(pos, pos + 5);
+                        number.append(n.substring(1));
+                        if (n.charAt(0) == '0') {
+                            stop = true;
+                        }
+                        pos += 5;
+                    } while (!stop);
+                    long n2 = Long.parseLong(number.toString(), 2);
+                    log.debug("number: {}", n2);
+                    result.output = binaryInput.substring(pos);
+                    result.value = n2;
+                    return result;
+
+                case 0:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding sum operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        do {
+                            r = decodePacket(r.output);
+                            result.value += r.value;
+                        } while (r.output.length() > 0);
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        for (int i = 0; i < length; i++) {
+                            r = decodePacket(r.output);
+                            result.value += r.value;
+                        }
+                        result.output = r.output;
                     }
-                    while (out.length() > 0);
-                } else {
-                    length = Integer.parseInt(binaryInput.substring(pos, pos + 11), 2);
-                    log.info("length: {}", length);
-                    pos += 11;
-                    out = binaryInput.substring(pos);
-                    for (int i = 0; i < length; i++) {
-                        out = decodePacket(out);
+                    return result;
+
+                case 1:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding mul operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    result.value = 1;
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        do {
+                            r = decodePacket(r.output);
+                            result.value *= r.value;
+                        } while (r.output.length() > 0);
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        for (int i = 0; i < length; i++) {
+                            r = decodePacket(r.output);
+                            result.value *= r.value;
+                        }
+                        result.output = r.output;
                     }
-                }
-                return out;
+                    return result;
+
+                case 2:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding min operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    result.value = Long.MAX_VALUE;
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        do {
+                            r = decodePacket(r.output);
+                            result.value = Math.min(result.value, r.value);
+                        } while (r.output.length() > 0);
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        for (int i = 0; i < length; i++) {
+                            r = decodePacket(r.output);
+                            result.value = Math.min(result.value, r.value);
+                        }
+                        result.output = r.output;
+                    }
+                    return result;
+
+                case 3:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding max operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    result.value = Long.MIN_VALUE;
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        do {
+                            r = decodePacket(r.output);
+                            result.value = Math.max(result.value, r.value);
+                        } while (r.output.length() > 0);
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        for (int i = 0; i < length; i++) {
+                            r = decodePacket(r.output);
+                            result.value = Math.max(result.value, r.value);
+                        }
+                        result.output = r.output;
+                    }
+                    return result;
+
+                case 5:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding greater than operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        r = decodePacket(r.output);
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value > r2.value) ? 1L : 0L;
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        r = decodePacket(r.output);
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value > r2.value) ? 1L : 0L;
+                        result.output = r2.output;
+                    }
+                    return result;
+
+                case 6:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding less than operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = new Result(binaryInput.substring(pos, pos + length));
+                        r = decodePacket(r.output);
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value < r2.value) ? 1L : 0L;
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = new Result(binaryInput.substring(pos));
+                        r = decodePacket(r.output);
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value < r2.value) ? 1L : 0L;
+                        result.output = r2.output;
+                    }
+                    return result;
+
+                case 7:
+                    lengthTypeId = binaryInput.charAt(pos++);
+                    log.debug("Decoding equal operator type {}, version {}, id: {}, in length: {}", lengthTypeId, version, id, binaryInput.length());
+                    if (lengthTypeId == '0') {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 15), 2);
+                        Result r = decodePacket(binaryInput.substring(pos, pos + length));
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value == r2.value) ? 1L : 0L;
+                        result.output = binaryInput.substring(pos + length);
+                    } else {
+                        length = Integer.parseInt(binaryInput.substring(pos, pos += 11), 2);
+                        Result r = decodePacket(binaryInput.substring(pos));
+                        Result r2 = decodePacket(r.output);
+                        result.value = (r.value == r2.value) ? 1L : 0L;
+                        result.output = r2.output;
+                    }
+                    return result;
             }
         }
-        return "";
+        return null;
     }
 
     int problem1() {
-        String out = decodePacket(binary);
+        decodePacket(binary);
         return versionNumbers;
     }
 
-    int problem2() {
-        return 0;
+    long problem2() {
+        return decodePacket(binary).value;
+    }
+
+    static class Result {
+        String output;
+        long value;
+
+        public Result() {
+        }
+
+        public Result(String output) {
+            this.output = output;
+            this.value = 0;
+        }
+
     }
 }
