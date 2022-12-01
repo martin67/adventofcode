1ba8b05ec59eb45079bb938f44d33bb58af6182a
Martin Hagelin <martin@hagelin.nu>
2022-12-01 06:44:52 +0100

    2022, day 1

---
+++ src/main/java/aoc/aoc2022/Day1CalorieCounting.java
@@ -0,0 +1,33 @@
+package aoc.aoc2022;
+
+import java.util.*;
+
+public class Day1CalorieCounting {
+    Map<Integer, Integer> elves = new HashMap<>();
+
+    public Day1CalorieCounting(List<String> inputLines) {
+        int elv = 1;
+        for (String line : inputLines) {
+            if (line.isEmpty()) {
+                elv++;
+            } else {
+                int calorie = Integer.parseInt(line);
+                elves.merge(elv, calorie, Integer::sum);
+            }
+        }
+    }
+
+    int problem1() {
+        return Collections.max(elves.values());
+    }
+
+    int problem2() {
+        List<Integer> sortedCalories = new ArrayList<>(elves.values());
+        sortedCalories.sort(Collections.reverseOrder());
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += sortedCalories.get(i);
+        }
+        return sum;
+    }
+}
