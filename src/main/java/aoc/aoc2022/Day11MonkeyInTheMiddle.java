d7ef422e1a66597b23cf7e9a9a3276735309dade
Martin Hagelin <martin@hagelin.nu>
2022-12-11 14:21:10 +0100

    2022, day 11

---
+++ src/main/java/aoc/aoc2022/Day11MonkeyInTheMiddle.java
@@ -0,0 +1,152 @@
+package aoc.aoc2022;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+@Slf4j
+public class Day11MonkeyInTheMiddle {
+
+    List<Monkey> monkeys = new ArrayList<>();
+    long lcd = 1;
+
+    public Day11MonkeyInTheMiddle(List<String> inputLines) {
+        // Create all monkeys in advance;
+        int numberOfMonkeys = inputLines.size() / 7 + 1;
+        log.info("{} monkeys", numberOfMonkeys);
+        for (int i = 0; i < numberOfMonkeys; i++) {
+            monkeys.add(new Monkey(i));
+        }
+        int index = 0;
+
+        for (String line : inputLines) {
+            if (line.startsWith("Monkey")) {
+                index = Integer.parseInt(line.split(" ")[1].substring(0, 1));
+            } else if (line.startsWith("  Starting items:")) {
+                String[] items = line.substring(18).split(",");
+                for (String item : items) {
+                    monkeys.get(index).items.push(Long.parseLong(item.trim()));
+                }
+            } else if (line.startsWith("  Operation:")) {
+                monkeys.get(index).operation = line.substring(13);
+            } else if (line.startsWith("  Test:")) {
+                long divisor = Long.parseLong(line.trim().split(" ")[3]);
+                monkeys.get(index).divisor = divisor;
+                lcd *= divisor;
+            } else if (line.startsWith("    If true:")) {
+                monkeys.get(index).trueTarget = monkeys.get(Integer.parseInt(line.trim().split(" ")[5]));
+            } else if (line.startsWith("    If false:")) {
+                monkeys.get(index).falseTarget = monkeys.get(Integer.parseInt(line.trim().split(" ")[5]));
+            }
+        }
+    }
+
+    long problem1() {
+        for (int i = 0; i < 20; i++) {
+            for (Monkey monkey : monkeys) {
+                log.debug("Monkey {}", monkey.id);
+                while (!monkey.items.isEmpty()) {
+                    monkey.inspect();
+                }
+            }
+        }
+        List<Monkey> sortedMonkeys = monkeys.stream().sorted(Comparator.comparing(Monkey::getInspections).reversed()).toList();
+        return sortedMonkeys.get(0).inspections * sortedMonkeys.get(1).inspections;
+    }
+
+    long problem2() {
+        for (int i = 0; i < 10000; i++) {
+            for (Monkey monkey : monkeys) {
+                while (!monkey.items.isEmpty()) {
+                    monkey.inspect2();
+                }
+            }
+        }
+        List<Monkey> sortedMonkeys = monkeys.stream().sorted(Comparator.comparing(Monkey::getInspections).reversed()).toList();
+        return sortedMonkeys.get(0).inspections * sortedMonkeys.get(1).inspections;
+    }
+
+    class Monkey {
+        int id;
+        Deque<Long> items = new ArrayDeque<>();
+        long divisor;
+        String operation;
+        Monkey trueTarget;
+        Monkey falseTarget;
+        long inspections = 0;
+
+        public Monkey(int id) {
+            this.id = id;
+        }
+
+        long operate(long old) {
+            String[] s = operation.split(" ");
+            long a;
+            long b;
+
+            if (s[2].equals("old")) {
+                a = old;
+            } else {
+                a = Long.parseLong(s[2]);
+            }
+
+            if (s[4].equals("old")) {
+                b = old;
+            } else {
+                b = Long.parseLong(s[4]);
+            }
+
+            return switch (s[3]) {
+                case "+" -> a + b;
+                case "*" -> a * b;
+                default -> throw new IllegalStateException("Unexpected value: " + s[4]);
+            };
+        }
+
+        void inspect() {
+            inspections++;
+            long item = items.pollLast();
+            log.debug("  Monkey inspects an item with a worry level of {}", item);
+
+            long worryLevel = operate(item);
+            log.debug("    Worry level is updated to {}", worryLevel);
+
+            worryLevel = worryLevel / 3;
+            log.debug("    Monkey gets bored with item. Worry level is divided by 3 to {}.", worryLevel);
+
+            if (worryLevel % divisor == 0) {
+                log.debug("    Current worry level is divisible by {}.", divisor);
+                log.debug("    Item with worry level {} is thrown to monkey {}", worryLevel, trueTarget.id);
+                trueTarget.items.push(worryLevel);
+            } else {
+                log.debug("    Current worry level is not divisible by {}.", divisor);
+                log.debug("    Item with worry level {} is thrown to monkey {}", worryLevel, falseTarget.id);
+                falseTarget.items.push(worryLevel);
+            }
+        }
+
+        void inspect2() {
+            inspections++;
+            long item = items.pollLast();
+            long worryLevel = operate(item);
+            if (worryLevel < 0) {
+                log.error("oops {}", worryLevel);
+            }
+
+            long rest = worryLevel / lcd;
+            if (rest >= 1) {
+                worryLevel -= rest * lcd;
+            }
+
+            if (worryLevel % divisor == 0) {
+                trueTarget.items.push(worryLevel);
+            } else {
+                falseTarget.items.push(worryLevel);
+            }
+        }
+
+        public long getInspections() {
+            return inspections;
+        }
+    }
+}
