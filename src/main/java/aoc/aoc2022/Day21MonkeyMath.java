6cf47c018c031ac7bd66a532ecbbc5ab9cbbfbf9
Martin Hagelin <martin@hagelin.nu>
2023-01-05 20:28:58 +0100

    2022, day 21

---
+++ src/main/java/aoc/aoc2022/Day21MonkeyMath.java
@@ -0,0 +1,125 @@
+package aoc.aoc2022;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class Day21MonkeyMath {
+    Map<String, Monkey> monkeys = new HashMap<>();
+
+    public Day21MonkeyMath(List<String> inputLines) {
+        Pattern pattern1 = Pattern.compile("(\\w+): (\\w+) (\\W) (\\w+)");
+        Pattern pattern2 = Pattern.compile("(\\w+): (\\d+)");
+        Matcher matcher;
+        for (String line : inputLines) {
+            matcher = pattern1.matcher(line);
+            if (matcher.find()) {
+                final String monkeyName = matcher.group(1);
+                Monkey start = monkeys.computeIfAbsent(monkeyName, m -> new Monkey(monkeyName));
+                final String monkeyA = matcher.group(2);
+                start.monkeyA = monkeys.computeIfAbsent(monkeyA, m -> new Monkey(monkeyA));
+                final String monkeyB = matcher.group(4);
+                start.monkeyB = monkeys.computeIfAbsent(monkeyB, m -> new Monkey(monkeyB));
+                start.operand = matcher.group(3);
+            }
+            matcher = pattern2.matcher(line);
+            if (matcher.find()) {
+                final String monkeyName = matcher.group(1);
+                Monkey start = monkeys.computeIfAbsent(monkeyName, m -> new Monkey(monkeyName));
+                start.number = Long.parseLong(matcher.group(2));
+            }
+        }
+    }
+
+    long problem1() {
+        Monkey root = monkeys.get("root");
+        return root.yell();
+    }
+
+    long problem2() {
+
+        //long x = bisection(0, Long.MAX_VALUE); // ok for problem1
+        long x = bisection(0, Long.MAX_VALUE / 10000); // ok for problem2
+        // Don't really understand why the end values has to be different. Is it something with the rounding,
+        // as we are not using floating numbers??
+        validate(x);
+        return x;
+    }
+
+    void validate(long x) {
+        Monkey monkeyA = monkeys.get("root").monkeyA;
+        Monkey monkeyB = monkeys.get("root").monkeyB;
+        Monkey humn = monkeys.get("humn");
+        humn.number = x;
+
+        log.info("Check. monkey A yell({}) = {}", x, monkeyA.yell());
+        log.info("Check. monkey B yell({}) = {}", x, monkeyB.yell());
+    }
+
+    long f(long x) {
+        Monkey monkeyA = monkeys.get("root").monkeyA;
+        Monkey monkeyB = monkeys.get("root").monkeyB;
+        Monkey humn = monkeys.get("humn");
+        humn.number = x;
+        return monkeyA.yell() - monkeyB.yell();
+    }
+
+    long bisection(long a, long b) {
+        long maxIterations = 10000;
+        long iterations = 0;
+        while (iterations < maxIterations) {
+            long c = (a + b) / 2;
+            long fc = f(c);
+            if (fc == 0) {
+                log.info("A f({}) = 0, {} iterations", c, iterations);
+                return c;
+            }
+
+            iterations++;
+            if (fc > 0) {
+                a = c;
+            } else {
+                b = c;
+            }
+        }
+        return -1;
+    }
+
+    static class Monkey {
+        String name;
+        long number;
+        Monkey monkeyA;
+        Monkey monkeyB;
+        String operand;
+
+        public Monkey(String name) {
+            this.name = name;
+        }
+
+        long yell() {
+            if (monkeyA == null && monkeyB == null) {
+                return number;
+            }
+            long a = monkeyA.yell();
+            long b = monkeyB.yell();
+            return switch (operand) {
+                case "+" -> Math.addExact(a, b);
+                case "-" -> Math.subtractExact(a, b);
+                case "*" -> Math.multiplyExact(a, b);
+                case "/" -> a / b;
+//                case "+" -> a + b;
+//                case "-" -> a - b;
+//                case "*" -> a * b;
+//                case "/" -> a / b;
+                default -> throw new IllegalArgumentException("Invalid operand: " + operand);
+            };
+        }
+    }
+
+}
+
