1c7b13d113667610d1276b62a7f7bec0d3e540b5
Martin Hagelin <martin@hagelin.nu>
2024-12-04 19:29:15 +0100

    2023, day 15

--- aoc/2023/src/main/java/aoc/aoc2023/Day15LensLibrary.java
+++ aoc/2023/src/main/java/aoc/aoc2023/Day15LensLibrary.java
@@ -1,42 +1,88 @@
 package aoc.aoc2023;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Slf4j
 class Day15LensLibrary {
     String input;
-    public Day15LensLibrary(List<String> inputLines) {
+
+    Day15LensLibrary(List<String> inputLines) {
         for (String line : inputLines) {
             input = line;
         }
     }
 
-    public int problem1() {
+    int problem1() {
         int sum = 0;
         for (String in : input.split(",")) {
             sum += hash(in);
         }
         return sum;
-        //return Arrays.stream(Arrays.stream(input.split(",")).mapToInt(this::hash).sum());
     }
 
-    public int problem2() {
-        return 0;
+    int problem2() {
+        Pattern pattern = Pattern.compile("(\\w+)(-|=)(\\d+)?,?");
+        var matcher = pattern.matcher(input);
+        List<LinkedList<Lens>> boxes = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+            boxes.add(new LinkedList<>());
+        }
+
+        while (matcher.find()) {
+            String tag = matcher.group(1);
+            String operation = matcher.group(2);
+            int focalLength = operation.equals("=") ? Integer.parseInt(matcher.group(3)) : -1;
+            int boxNumber = hash(tag);
+            Lens newLens = new Lens(tag, focalLength);
+            var box = boxes.get(boxNumber);
+
+            if (operation.equals("-")) {
+                box.removeIf(lens -> lens.tag.equals(newLens.tag));
+            } else {
+                Optional<Lens> existingLens = box.stream().filter(lens -> lens.tag.equals(newLens.tag)).findFirst();
+                if (existingLens.isPresent()) {
+                    existingLens.get().focalLength = focalLength;
+                } else {
+                    box.add(newLens);
+                }
+
+            }
+        }
+
+        int power = 0;
+        for (int i = 0; i < boxes.size(); i++) {
+            var box = boxes.get(i);
+            for (int j = 0; j < box.size(); j++) {
+                var lens = box.get(j);
+                power += (i + 1) * (j + 1) * lens.focalLength;
+            }
+        }
+        return power;
     }
 
     int hash(String in) {
         int currentValue = 0;
         for (char c : in.toCharArray()) {
-            int a = c;
-            currentValue += a;
+            currentValue += c;
             currentValue *= 17;
             currentValue = currentValue % 256;
-
         }
         return currentValue;
+    }
+
+    static class Lens {
+        String tag;
+        int focalLength;
+
+        public Lens(String tag, int focalLength) {
+            this.tag = tag;
+            this.focalLength = focalLength;
+        }
     }
 }
--- aoc/2023/src/test/java/aoc/aoc2023/Day15LensLibraryTest.java
+++ aoc/2023/src/test/java/aoc/aoc2023/Day15LensLibraryTest.java
@@ -1,12 +1,11 @@
 package aoc.aoc2023;
 
+import aoc.common.AocFiles;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
-
-import aoc.common.AocFiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,15 +15,15 @@ class Day15LensLibraryTest {
     @ParameterizedTest
     @CsvSource({"52, day15-demo1.txt",
             "1320, day15-demo2.txt",
-            "0, day15.txt"})
+            "514394, day15.txt"})
     void problem1(int expected, String fileName) throws IOException {
         var inputLines = AocFiles.readAllLines(fileName);
         assertThat(new Day15LensLibrary(inputLines).problem1()).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"0, day15-demo1.txt",
-            "0, day15.txt"})
+    @CsvSource({"145, day15-demo2.txt",
+            "236358, day15.txt"})
     void problem2(int expected, String fileName) throws IOException {
         var inputLines = AocFiles.readAllLines(fileName);
         assertThat(new Day15LensLibrary(inputLines).problem2()).isEqualTo(expected);
